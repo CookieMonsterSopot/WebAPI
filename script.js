dec9d02fe82f6678ee7a60241c75fa5c9b763b7a
Kacper Żarna <kacper@192.168.1.16>
2022-12-04 11:59:50 +0100

    Kod 12:00 4.12.2022

--- script.js
+++ script.js
@@ -15,6 +15,45 @@ const navH1 = document.querySelector("#nav-h1");
 
 // - class
 const firstNavSpan = document.querySelector(".nav-span");
-console.log(firstNavSpan);
+// console.log(firstNavSpan);
 
 // - tagname
+const navElement = document.querySelector("nav");
+// console.log(navElement);
+
+// b) document.querySelectorAll()
+// Przyjmuje jako argument selektor CSSowy, zwraca NodeList, jeżeli nie znajdzie nic to zwraca null.
+const navSpans = document.querySelectorAll(".nav-span");
+//console.log(navSpans);
+// console.log(navSpans[2]);
+// const navSpansArray = [...navSpans];
+// console.log(navSpansArray);
+//navSpansArray.map((el) => el.id);
+
+//[1, 2, 3, 4] => ...[1, 2, 3, 4] => 1, 2, 3, 4
+//const arr = [1, 2, 3, 4];
+// console.log(arr);
+// console.log(arr[0], arr[1], arr[2], arr[3]);
+// console.log(...arr);
+// const add = (a, b, c, d) => a + b + c + d;
+// console.log(add(...arr));
+
+// c) document.getElementById()
+// Metoda przyjmuje stringa z id, zwraca element o danym id, jeżeli nic nie znajdzie zwróci null.
+const contentDiv = document.getElementById("content");
+// console.log(contentDiv);
+
+// d) document.getElementsByClassName()
+// Metoda przyjmuje stringa z klasą, zwraca HTMLCollection, jeżeli nie znajdzie żadnego elementu zwraca null.
+// const articleParagraphs = [
+//   ...document.getElementsByClassName("article-paragraph"),
+// ];
+//const articleParagraphs = document.getElementsByClassName("article-paragraph");
+
+// console.log(articleParagraphs);
+
+// 2. SUB-SELEKTORY
+
+// a) element.children
+console.log(contentDiv);
+console.log(contentDiv.children);
