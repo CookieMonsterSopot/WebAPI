4b7ab1c46598f62c0683f3d32ff6910713cab81a
Kacper Żarna <kacper@192.168.1.16>
2022-12-04 15:00:55 +0100

    Kod 15:00 4.12.2022

--- script.js
+++ script.js
@@ -80,14 +80,59 @@ const middleLi = list.children[1];
 // Zapisz w zmiennych wszystkie elementy występujące w naszym dokumencie HTML. Staraj się nie modyfikować samego HTMLa, lecz jeżeli będzie to konieczne, zrób to.
 
 const body = document.querySelector("body");
-console.log(body);
+// console.log(body);
 const nav = document.querySelector("nav");
 const articles = contentDiv.children;
 const firstArticleH2 = articles[0].firstElementChild;
 const firstArticleP = articles[0].lastElementChild;
 const listItems = list.children;
-console.log(nav);
-console.log(articles);
-console.log(firstArticleH2);
-console.log(firstArticleP);
-console.log(listItems);
+// console.log(nav);
+// console.log(articles);
+// console.log(firstArticleH2);
+// console.log(firstArticleP);
+// console.log(listItems);
+
+// 3. TWORZENIE I DODAWANIE ELEMENTÓW
+
+// a) document.createElement()
+// createElement jest funkcją która tworzy elementy jako obiekty JSowe. Ten obiekt możemy dowolnie modyfikować i wstawiać do DOM aby wyświetlić element na stronie. Jako argument przyjmuje nazwe tagu w stringu.
+const additionalLi = document.createElement("li");
+additionalLi.textContent = "This was appended using .appendChild() method.";
+additionalLi.id = "additional-li";
+console.log(additionalLi);
+//<li id="additional-li">This was appended using .appendChild() method.</li>
+
+// b) element.appendChild()
+// appendChild to metoda do wstawiania elementów stworzonych w JSie do DOM. Wywołuje się tą metode na elemencie do którego chcemy wstawić nowy element, a jako argument podajemy element do wstawienia (element stworzony w JSie)
+
+// list to zmienna przechowujaca selektor do ul'a na stronie (jest wyżej), linia 65
+list.appendChild(additionalLi);
+
+// c) element.insertBefore()
+// Metoda doda nowy element (1 argument) przed element podany w drugim argumencie. Metoda wywoływana na elemencie w którym mają zajść zmiany
+const additionalLi2 = document.createElement("li");
+additionalLi2.textContent = "Nowe LI, wstawione przy pomocy insertBefore()";
+const firstLi = list.firstElementChild;
+list.insertBefore(additionalLi2, firstLi);
+
+// d) string + insertAdjacentHTML()
+const html = `<li id="li-to-remove">Li to remove</li>`;
+list.insertAdjacentHTML("beforeend", html);
+
+// 4. USUWANIE ELEMENTÓW
+
+// a) Czyszczenie zawartości elementów przy pomocy innerHTML. Wyczyści wszystko pomiędzy tagami danego elementu, ale tagi zostawi.
+const liToRemove = document.getElementById("li-to-remove");
+// liToRemove.innerHTML = "";
+// <li id="li-to-remove">Li to remove <p>123321</p></li>
+// *innerHTML = ''*
+/* <li id="li-to-remove"></li> */
+
+// b) element.remove()
+// Ta metoda po prostu usuwa element na którym została wywołana, usuwa cały element z tagami włącznie
+liToRemove.remove();
+
+// c) element.removeChild()
+// Metoda usuwa wskazany element dziecko elementu na którym została wywołana.
+// list to wyżej zdefiniowana zmienna
+list.removeChild(list.firstElementChild);
